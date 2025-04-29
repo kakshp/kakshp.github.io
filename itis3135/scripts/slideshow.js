5632d1e69fa458715347c25a85e8ecfc14545345
kakshp <kpate253@charlotte.edu>
2025-04-28 20:50:44 -0400

    change

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    const thumbnails = $(".thumb");
+    let currentIndex = 0;
+
+    function showImage(index) {
+        const selectedThumb = thumbnails.eq(index);
+        const newSrc = selectedThumb.attr("src");
+        const newAlt = selectedThumb.attr("alt");
+
+        $("#mainImage").attr("src", newSrc);
+        $("#mainImage").attr("alt", newAlt);
+        $("#caption").text(newAlt);
+        currentIndex = index;
+    }
+
+    // Thumbnail click
+    thumbnails.click(function () {
+        currentIndex = thumbnails.index(this);
+        showImage(currentIndex);
+    });
+
+    // Next button
+    $("#nextBtn").click(function () {
+        currentIndex = (currentIndex + 1) % thumbnails.length;
+        showImage(currentIndex);
+    });
+
+    // Prev button
+    $("#prevBtn").click(function () {
+        currentIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
+        showImage(currentIndex);
+    });
+
+    $(function () {
+        $("#header").load("components/header.html");
+        $("#footer").load("components/footer.html");
+    });
+});
